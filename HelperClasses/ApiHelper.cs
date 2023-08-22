6785132f3a579d8e57c0e4cc7431916a8a4ec9da
Mahabub Rony <65343674+MahabubRony16@users.noreply.github.com>
2023-08-22 18:50:31 +0600

    Changed

--- HelperClasses/ApiHelper.cs
+++ HelperClasses/ApiHelper.cs
@@ -1,8 +1,17 @@
 ﻿using System;
-
+using System.Net.Http;
+using System.Net.Http.Headers;
 namespace HelperClasses
 {
     public class ApiHelper
     {
+        public static HttpClient ApiClient { get; set; }
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
     }
 }
